8df30a2430ca171fb5b58d91c8cfb090c9ee1a24
Ganesh Patil <ganesh.p@neosoftmail.com>
2024-08-01 17:00:02 +0530

    Initial Commit

---
+++ CarGoSwiftUI/CarGoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarGoSwiftUIApp.swift
+//  CarGoSwiftUI
+//
+//  Created by Neosoft on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CarGoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
